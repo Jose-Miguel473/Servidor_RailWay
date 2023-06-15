d6c53c11d2fa571f2d79fec3f68af1a11d0861c0
Jose-Miguel473 <josecarballo120@gmail.com>
2023-06-15 04:13:43 -1200

    #28 FUNCION COMPARATIVECALL ACTUALIZADA PARA CONTADOR DE LLAMDAS

--- src/controllers/filter.controller.js
+++ src/controllers/filter.controller.js
@@ -87,8 +87,8 @@ return data
       
       if(callCount2 > 0){
       result.push({
-        source: userDevice2,
-        target: commonNumber,
+        source: commonNumber ,
+        target: userDevice2,
         count: callCount2,
         namesContactsFromUsers1: nameContact1,
         namesContactsFromUsers2: nameContact2,
