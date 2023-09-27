4bdd1004fbbc86ff58533437c2cb91f4f2b601ef
Jose-Miguel473 <josecarballo120@gmail.com>
2023-09-26 17:31:54 -1200

    #28 FUNCION COMPARATIVECALL ACTUALIZADA PARA REDUCIR TIEMPOR DE ENVIO DE DATOS

--- src/controllers/filter.controller.js
+++ src/controllers/filter.controller.js
@@ -44,16 +44,14 @@ return data
   const archUser2 = await getCallLogs(idUser2);
   const result = [];
 
-  const callCounts = {};
-
-  Calls.forEach(({ number, type,userDevice }) => {
+  const callCounts = Calls.reduce((acc, { number, type, userDevice }) => {
     if (type !== "REJECTED_TYPE" && type !== "MISSED_TYPE") {
       const phoneNumber = number.replace("+591", "");
-      callCounts[phoneNumber] = callCounts[phoneNumber] || {};
-      callCounts[phoneNumber][userDevice] = callCounts[phoneNumber][userDevice] || 0;
-      callCounts[phoneNumber][userDevice]++;
+      acc[phoneNumber] = acc[phoneNumber] || {};
+      acc[phoneNumber][userDevice] = (acc[phoneNumber][userDevice] || 0) + 1;
     }
-  });
+    return acc;
+  }, {});
 
   const setArchUser1 = new Set(archUser1.map(({ number }) => number.replace("+591", "")));
   const setArchUser2 = new Set(archUser2.map(({ number }) => number.replace("+591", "")));
@@ -106,145 +104,6 @@ return data
   return result;
 };
 
- const getCallLogs = async (userDevice) => {
-  
-  const CallLogs = await CallLog.find({ userDevice })
-  const CallInfo = []
-  let totalDuration = 0
-
-  CallLogs.map(({ userDevice, nameContact, number, duration, date, type }) => {
-          
-    CallInfo.push({ 
-      userDevice: `${userDevice}`,
-      nameContact: `${nameContact}`,
-      number: `${number}`.replace("+591",""),
-      duration: `${duration}`,
-      type:`${type}`,
-      date:`${date}`
-    })
-  
-  })
-  return CallInfo
-}
-
-const getUserDeviceById = async (userDeviceId) => {
-  //const userDeviceId = req.params.id;
-  try {
-    const userDevice = await UserDevice.findById(userDeviceId);
-    if (!userDevice) {
-      console.log("UserDevice no existe por ese id")
-   
-    }
-    return userDevice
-
-  } catch (error) {
-    console.log(error);
-   
-  }
-}
-
-
-const getAllUser = async (req, res = response) => {
-  try {
-    const [users, calls] = await Promise.all([
-      UserDevice.find(),
-      CallLog.find()
-    ]);
-
-    const userMap = new Map(users.map(({ _id, nameUser }) => [`${_id}`, nameUser]));
-
-    const callCounts = new Map();
-    const link = [];
-    const result = [];
-    const nodos = [];
-
-    const comparativeCalls = await Promise.all(
-      users.map(async (user, i) => {
-        const userId1 = user._id;
-        const userId2s = users.slice(i + 1).map(u => u._id);
-
-        const comparativeContacts = await Promise.all(
-          userId2s.map(userId2 => getComparativeCall(userId1, userId2))
-        );
-
-        return comparativeContacts.flat();
-      })
-    );
-
-    for (const comparativeContacts of comparativeCalls) {
-      result.push(...comparativeContacts);
-    }
-
-   
-
-    for (const call of calls) {
-      const { number, type,userDevice } = call;
-      const phoneNumber = `${number}`.replace("+591", "");
-
-      if (type === "OUTGOING_TYPE" || type === "INCOMING_TYPE" ) {
-        callCounts.set(phoneNumber, (callCounts.get(phoneNumber) || 0) + 1);
-      }
-
-      const callCount = callCounts.get(phoneNumber) || 0;
-
-      if (type !== "REJECTED_TYPE" && type !== "MISSED_TYPE") {
-        
-        link.push({
-          source: call.userDevice,
-          target: phoneNumber,
-          type,
-          count: callCount
-        });
-      }
-    }
-
-  const nodosUpdate = calls;
-   //const linkUpdate = OrderforSort(calls);
-
-    for (const { nameContact, number,type } of nodosUpdate) {
-      
-      if (type !== "REJECTED_TYPE" && type !== "MISSED_TYPE") {
-        
-      nodos.push({
-        userDevice: `${number}`.replace("+591", ""),
-        name: nameContact
-      });
-      }
-    }
-
-    const ContactOrder = OneContact(nodos);
-    const ContacLink = OneTarget(link);
-    const OneResult = OneTarget(result);
-
-    const node = ContactOrder.concat(
-      users.map(({ _id }) => ({
-        userDevice: _id,
-        name: userMap.get(`${_id}`)
-      }))
-    );
-
-    const links = ContacLink.concat(OneResult);
-
-    const data = {
-      nodes: node,
-      links: links
-    };
-
-    return res.status(200).json({
-      transaction: true,
-      code: 0,
-      data
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      transaction: false,
-      code: -2,
-      msg: "Excepcion No Controlada, por favor informe al administrador."
-    });
-  }
-};
-
 const ComparativeCall = async (req, res = response) => {
   try {
     const Calls = await CallLog.find();
@@ -289,7 +148,7 @@ const ComparativeCall = async (req, res = response) => {
               result.push({
                 userID1: userDevice1,
                 number1: `${number1}`.replace("+591", ""),
-                userName1: userName.nameUser, // Corregir el nombre de la propiedad
+                userName1: userName.nameUser, 
                 namesContactsFromUsers1: `${nameContact1}`,
                 count1: callCount1,
                 userID2: userDevice2,
@@ -319,6 +178,150 @@ const ComparativeCall = async (req, res = response) => {
       transaction: false,
       code: -2, // Excepcion no controllada
       msg: "Excepcion No Controlada, por favor informe al administrador.",
+    });
+  }
+};
+ const getCallLogs = async (userDevice) => {
+  
+  const CallLogs = await CallLog.find({ userDevice })
+  const CallInfo = []
+  let totalDuration = 0
+
+  CallLogs.map(({ userDevice, nameContact, number, duration, date, type }) => {
+          
+    CallInfo.push({ 
+      userDevice: `${userDevice}`,
+      nameContact: `${nameContact}`,
+      number: `${number}`.replace("+591",""),
+      duration: `${duration}`,
+      type:`${type}`,
+      date:`${date}`
+    })
+  
+  })
+  return CallInfo
+}
+
+const getUserDeviceById = async (userDeviceId) => {
+  //const userDeviceId = req.params.id;
+  try {
+    const userDevice = await UserDevice.findById(userDeviceId);
+    if (!userDevice) {
+      console.log("UserDevice no existe por ese id")
+   
+    }
+    return userDevice
+
+  } catch (error) {
+    console.log(error);
+   
+  }
+}
+
+
+const getAllUser = async (req, res = response) => {
+  try {
+    const [users, calls] = await Promise.all([
+      UserDevice.find(),
+      CallLog.find()
+    ]);
+    const nodeSet = new Set();
+    const userMap = new Map(users.map(({ _id, nameUser }) => [`${_id}`, nameUser]));
+
+    const callCounts = new Map();
+    const link = [];
+    const result = [];
+    const nodos = [];
+
+    const comparativeCalls = await Promise.all(
+      users.map(async (user, i) => {
+        const userId1 = user._id;
+        const userId2s = users.slice(i + 1).map(u => u._id);
+    
+        const comparativeContacts = await Promise.all(
+          userId2s.map(userId2 => getComparativeCall(userId1, userId2))
+        );
+    
+        return comparativeContacts.flat();
+      })
+    );
+    
+
+    for (const comparativeContacts of comparativeCalls) {
+      result.push(...comparativeContacts);
+    }
+
+   
+
+    for (const call of calls) {
+      const { number, type,userDevice } = call;
+      const phoneNumber = `${number}`.replace("+591", "");
+
+      if (type === "OUTGOING_TYPE" || type === "INCOMING_TYPE" ) {
+        callCounts.set(phoneNumber, (callCounts.get(phoneNumber) || 0) + 1);
+      }
+
+      const callCount = callCounts.get(phoneNumber) || 0;
+
+      if (type !== "REJECTED_TYPE" && type !== "MISSED_TYPE") {
+        
+        link.push({
+          source: call.userDevice,
+          target: phoneNumber,
+          type,
+          count: callCount
+        });
+      }
+    }
+
+  const nodosUpdate = calls;
+   //const linkUpdate = OrderforSort(calls);
+
+    for (const { nameContact, number,type } of nodosUpdate) {
+      
+      if (type !== "REJECTED_TYPE" && type !== "MISSED_TYPE") {
+        const phoneNumber = `${number}`.replace("+591", "");
+    
+        // Verificar si el nodo no existe en el conjunto antes de agregarlo
+        if (!nodeSet.has(phoneNumber)) {
+          nodeSet.add(phoneNumber);  
+      nodos.push({
+        userDevice: `${number}`.replace("+591", ""),
+        name: nameContact
+      });
+      }
+    }
+    }
+
+    const ContactOrder = OneContact(nodos);
+    const ContacLink = OneTarget(link);
+    const OneResult = OneTarget(result);
+
+    const node = ContactOrder.concat(
+      users.map(({ _id }) => ({
+        userDevice: _id,
+        name: userMap.get(`${_id}`)
+      }))
+    );
+
+    const links = ContacLink.concat(OneResult);
+
+    const data = {
+      nodes: node,
+      links: links
+    };
+
+    return res.status(200).json({
+      transaction: true,
+      code: 0,
+      data
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      transaction: false,
+      code: -2,
+      msg: "Excepcion No Controlada, por favor informe al administrador."
     });
   }
 };
